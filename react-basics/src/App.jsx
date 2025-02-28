eecfd785d71d021e1a388e41a90e5de3992c7854
harshita <harshita@logicrays.com>
2025-02-28 13:07:00 +0530

    firstcommit

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,18 @@
+// import './App.css'
+
+import About from "./components/about"
+import Home from "./components/Home"
+
+function App() {
+   return (
+    <>
+      <h1>Hello React</h1>
+      {/* <h2>Welcome to LRA</h2> */}
+      <Home></Home>
+      <About/>
+    </>
+
+  )
+}
+
+export default App
---
+++ react-basics/src/components/Home.jsx
@@ -0,0 +1,33 @@
+// function Home(){
+//     return (<></>)
+// }
+
+// const Home = ()=>{
+//     return (<></>)
+// }
+// export default Home
+
+//rfc 
+// import React from 'react'
+// export default function Home() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+//rafc
+import React from 'react'
+import About from './about'
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <About></About>
+    </>
+  )
+}
+
+export default Home
